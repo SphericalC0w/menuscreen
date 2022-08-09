9dd1270dae39fd68f79cfff3c225642a50befef6
SphericalC0w <110639991+SphericalC0w@users.noreply.github.com>
2022-08-08 23:19:02 -0700

    Add files via upload
     ver 1 that just makes it R/G/B

---
+++ menu attemp 2.py
@@ -0,0 +1,88 @@
+import pygame
+
+pygame.init()
+running = True
+screen = pygame.display.set_mode((1280, 720))
+surface = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption('Menu screen')
+object1 = pygame.Rect((426, 100), (754, 133))
+object2 = pygame.Rect((426, 293), (754, 133))
+object3 = pygame.Rect((426, 486), (754, 133))
+menu_photo = pygame.image.load(r'C:\Users\SphericalCow\Desktop\coding\menu\menu.png')
+red = pygame.image.load(r'C:\Users\SphericalCow\Desktop\coding\menu\red.png')
+green = pygame.image.load(r'C:\Users\SphericalCow\Desktop\coding\menu\green.png')
+blue = pygame.image.load(r'C:\Users\SphericalCow\Desktop\coding\menu\blue.png')
+menu1 = False
+menu2 = False
+menu3 = False
+	
+pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(426, 100, 754, 133))
+pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(426, 293, 754, 133))
+pygame.draw.rect(surface, (255, 255, 255), pygame.Rect(426, 486, 754, 133))
+screen.blit(menu_photo, (100, 0))
+screen.blit(red, (426, 100))
+screen.blit(green, (426, 293))
+screen.blit(blue, (426, 486))
+pygame.display.flip()
+
+while running:
+    if pygame.mouse.get_pressed()[0] and object1.collidepoint(pygame.mouse.get_pos()):
+        menu1 = True
+    if pygame.mouse.get_pressed()[0] and object2.collidepoint(pygame.mouse.get_pos()):
+        menu2 = True    
+    if pygame.mouse.get_pressed()[0] and object3.collidepoint(pygame.mouse.get_pos()):
+        menu3 = True    
+   
+    if menu1 == True:
+        screen.fill((255, 0, 0))
+        pygame.display.update()
+        break
+    if menu2 == True:
+        screen.fill((0, 255, 0))
+        pygame.display.update()
+        break
+    if menu3 == True:
+        screen.fill((0, 0, 255))
+        pygame.display.update()
+        break
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #try:
+        #if keyboard.is_pressed('q'):
+             #menu1 = True
+             #break
+        #if keyboard.is_pressed('2'):
+             #menu2 = True
+             #break
+        #if keyboard.is_pressed('3'):
+             #menu3 = True
+             #break
+    #except:
+        #break
+
+            
+
